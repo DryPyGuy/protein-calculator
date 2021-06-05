e9d91c4badc73ea1673f5eec7a689fb34f3a06b2
DryPyGuy <84981364+DryPyGuy@users.noreply.github.com>
2021-06-05 17:40:54 -0400

    Update protein_calc.py

--- protein_calc.py
+++ protein_calc.py
@@ -1,5 +1,10 @@
 
 print("Hi, there!")
 weight = input("So, you're trying to figure out how much protein you should consume to gain muscle mass? Input your weight here:\n")
-rec_protein_intake = float(weight) * .65
-print(f"Eat {rec_protein_intake} grams of protein daily to gain muscle mass.")
+
+try:
+	rec_protein_intake = float(weight) * .65
+except ValueError:
+	print("Oops, that doesnt look like a valid weight")
+else:	
+	print(f"Eat {rec_protein_intake} grams of protein daily to gain muscle mass.")
