7fb28ad04747963145d9d21a07b4c7cb8eeacdc6
DryPyGuy <84981364+DryPyGuy@users.noreply.github.com>
2021-06-05 17:29:54 -0400

    Basic app

---
+++ protein_calc.py
@@ -0,0 +1,5 @@
+
+print("Hi, there!")
+weight = input("So, you're trying to figure out how much protein you should consume to gain muscle mass? Input your weight here:\n")
+rec_protein_intake = float(weight) * .65
+print(f"Eat {rec_protein_intake} grams of protein daily to gain muscle mass.")
